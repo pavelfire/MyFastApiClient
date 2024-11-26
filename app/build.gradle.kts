5791ac964beefd86ac7dd657b91cf2675607c505
pvdo <fire_ra@bk.ru>
2024-11-26 23:57:13 +0300

    moved dependencies to toml file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,9 +60,9 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.logging.interceptor)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
